3b75519f7eb81acbc304a7673f429769ef396229
Praise25 <anenepraise1@gmail.com>
2022-10-25 20:29:09 +0100

    feat: added socials icons

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import ProfileHeader from "./ProfileHeader"
 import LinkList from "./LinkList";
+import Socials from "./Socials";
 
 const Profile = () => {
   return (
     <Fragment>
       <ProfileHeader />
       <LinkList />
+      <Socials />
     </Fragment>
   )
 }
