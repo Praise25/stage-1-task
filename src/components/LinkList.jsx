8338b7b89a9205b8e1c874e2bf8d7e7f80dcdd9a
Praise25 <anenepraise1@gmail.com>
2022-10-25 20:03:58 +0100

    refactor(links-section): replaced fragment wrapper with section

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,16 +1,15 @@
-import { Fragment } from "react";
 import LinkItems from "./LinkItems";
 
 const LinkList = () => {
   return (
-    <Fragment>
+    <section>
       <LinkItems name="Twitter Link" link="https://twitter.com/PraiseTheDev" />
       <LinkItems name="Zuri Team" link="https://training.zuri.team/" id="btn__zuri" />
       <LinkItems name="Zuri Books" link="http://books.zuri.team" id="books" />
       <LinkItems name="Python Books" link="https://books.zuri.team/python-for-beginners?ref_id=praise" id="book__python" />
       <LinkItems name="Background Check for Coders" link="https://background.zuri.team" id="pitch" />
       <LinkItems name="Design Books" link="https://books.zuri.team/design-rules" id="book__design" />
-    </Fragment>
+    </section>
   );
 };
 
