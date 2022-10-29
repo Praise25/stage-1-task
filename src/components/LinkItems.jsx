070630f588afe1b8a64fc5b59b9687ab8cf65d8e
Praise25 <anenepraise1@gmail.com>
2022-10-29 21:34:19 +0100

    feat: add description to some links

--- src/components/LinkItems.jsx
+++ src/components/LinkItems.jsx
@@ -3,9 +3,13 @@ import classes from "./LinkItems.module.css";
 const LinkItems = (props) => {
   return (
     <div className={classes["link-item"]}>
-     <a href={props.link} id={props.id}>{props.name}</a>
+      <a href={props.link} id={props.id}>
+        {props.name}
+        <br />
+        <sub>{props.sub}</sub>
+      </a>
     </div>
-  )
-}
+  );
+};
 
 export default LinkItems;
